5896ea76f0973711339ac0abb9bb73e4d3d353d5
Miguel Nievas <miguelnievas[at]ucm[dot]es>
2016-05-29 14:09:31 -0400

    Some tools to make the life of any poor & geeky linux user easier

---
+++ 2screen.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+add_modeline() {
+	local modeline name
+	modeline="$(gtf "$2" "$3" "$4" | sed -n 's/.*Modeline "\([^" ]\+\)" \(.*\)/\1 \2/p')"
+	name="$(echo "${modeline}" | sed 's/\([^ ]\+\) .*/\1/')"
+	if [ -z "${modeline}" -o -z "${name}" ] ; then
+		echo "Error! modeline='${modeline}' name='${name}'"
+		exit 1
+	fi
+	xrandr --delmode "$1" "${name}"
+	xrandr --rmmode "${name}"
+	xrandr --newmode ${modeline}
+	xrandr --addmode "$1" "${name}"
+}
+
+add_modeline VIRTUAL1 960 540 60
+add_modeline VIRTUAL1 1280 768 60
+add_modeline VIRTUAL1 1920 1080 60
+add_modeline VIRTUAL1 1920 1200 60
+add_modeline VIRTUAL1 960 600 60
+add_modeline VIRTUAL1 800 480 60
+add_modeline VIRTUAL1 640 400 60
+add_modeline VIRTUAL1 640 360 60
+
+dconf write /org/gnome/settings-daemon/plugins/media-keys/video-out ''
+dconf write /org/gnome/settings-daemon/plugins/media-keys/screenshot ''
+dconf write /org/gnome/settings-daemon/plugins/xrandr/active false
+#dconf write /org/gnome/settings-daemon/plugins/media-keys/active false
+
+
+xrandr --output eDP1 --auto --primary --output VIRTUAL1 --auto --secondary --left-of eDP1
+
+x11vnc -clip xinerama1
---
+++ initSoftAP.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ "$1" == "-f" ]; then 
+  rm -rf /tmp/hostapd.lock
+fi
+
+if [ -f /tmp/hostapd.lock ]; then 
+  echo "/tmp/hostapd.lock exists, remove it before running this" 
+  exit 1
+fi
+
+nmcli radio wifi off
+sleep 2
+rfkill unblock wlan
+sleep 2
+#Initial wifi interface configuration
+ifconfig wlp1s0 up 10.0.0.1 netmask 255.255.255.0
+sleep 2
+###########Start DHCP, comment out / add relevant section##########
+#Thanks to Panji
+#Doesn't try to run dhcpd when already running
+if [ "$(ps -e | grep dhcpd)" == "" ]; then
+dhcpd wlp1s0 &
+fi
+###########
+#Enable NAT
+iptables --flush
+iptables --table nat --flush
+iptables --delete-chain
+iptables --table nat --delete-chain
+iptables --table nat --append POSTROUTING --out-interface enp0s20u1u1 -j MASQUERADE
+iptables --append FORWARD --in-interface wlp1s0 -j ACCEPT
+ 
+#Thanks to lorenzo
+#Uncomment the line below if facing problems while sharing PPPoE, see lorenzo's comment for more details
+#iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+
+sleep 1
+ 
+sysctl -w net.ipv4.ip_forward=1
+#start hostapd
+hostapd /etc/hostapd/hostapd.conf 1>/dev/null 2>&1 &
+#killall dhcpd
+
+touch /tmp/hostapd.lock
